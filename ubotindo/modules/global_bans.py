e51a1fa42cf0ef1be5a49fb46c4dd380c3662007
Mr.Miss <keselekpermen69@gmail.com>
2020-08-29 21:04:53 +0700

    global_bans: change gban appeal chat
    Signed-off-by: Mr.Miss <keselekpermen69@gmail.com>

--- ubotindo/modules/global_bans.py
+++ ubotindo/modules/global_bans.py
@@ -409,7 +409,7 @@ def __user_info__(user_id):
         user = sql.get_gbanned_user(user_id)
         if user.reason:
             text += "\nReason: {}".format(html.escape(user.reason))
-            text += "\n\nAppeal at @userbotindo if you think it's invalid."
+            text += "\n\nAppeal at @userbotspamgroup if you think it's invalid."
     else:
         text = text.format("No")
     return text
