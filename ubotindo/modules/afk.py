4ec0b662375da487a080d011a1aad29d0ccabbe7
Tomy <tomy.prs25@gmail.com>
2020-09-07 22:29:48 +0700

    afk.py: Brb mean , Be right Back Fastest, Maybe
    Signed-off-by: Tomy <tomy.prs25@gmail.com>

--- ubotindo/modules/afk.py
+++ ubotindo/modules/afk.py
@@ -155,7 +155,7 @@ When marked as AFK, any mentions will be replied to with a message to say you're
 
 AFK_HANDLER = DisableAbleCommandHandler("afk", afk)
 AFK_REGEX_HANDLER = DisableAbleMessageHandler(
-    Filters.regex(r"(?i)^brb"), afk, friendly="afk"
+    Filters.regex("(?i)brb"), afk, friendly="afk"
 )
 NO_AFK_HANDLER = MessageHandler(Filters.all & Filters.group, no_longer_afk)
 AFK_REPLY_HANDLER = MessageHandler(Filters.all & Filters.group & ~Filters.update.edited_message, reply_afk)
