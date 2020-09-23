3ae5eceef1b1e2aeff90f724eb9ecec8f573638a
Tomy <tomy.prs25@gmail.com>
2020-09-23 11:33:50 +0700

    Revert "notes.py: Don't Use Notename if Missing Caption"
    This reverts commit f07ac7750f9e7bbb0901a400391d269b3d640834.
    
    This Fix Issue When Import
    
    Signed-off-by: Tomy <tomy.prs25@gmail.com>

--- ubotindo/modules/notes.py
+++ ubotindo/modules/notes.py
@@ -274,6 +274,9 @@ def save(update, context):
         msg.reply_text("Bruh! there's no note")
         return
 
+    if len(text.strip()) == 0:
+        text = note_name
+        
     sql.add_note_to_db(
         chat_id, note_name, text, data_type, buttons=buttons, file=content
     )
