8dd720f4c62fa10172a7cb6236e7cd47a38f6630
Tomy <tomy.prs25@gmail.com>
2020-09-23 12:01:39 +0700

    purge.py: Add time Taken for purge action
    Signed-off-by: Tomy <tomy.prs25@gmail.com>

--- ubotindo/modules/purge.py
+++ ubotindo/modules/purge.py
@@ -15,6 +15,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import asyncio
+import time
 
 from telethon import events
 from telethon.errors.rpcerrorlist import MessageDeleteForbiddenError
@@ -38,6 +39,7 @@ async def is_administrator(user_id: int, message):
 
 @client.on(events.NewMessage(pattern="^/purge"))
 async def purge(event):
+    start = time.perf_counter_ns()
     chat = event.chat_id
     msgs = []
 
@@ -53,6 +55,8 @@ async def purge(event):
     try:
         msg_id = msg.id
         count = 0
+        end= time.perf_counter_ns()
+        time_taken = (end - start) / (10**9)
         to_delete = event.message.id - 1
         await event.client.delete_messages(chat, event.message.id)
         msgs.append(event.reply_to_msg_id)
@@ -65,7 +69,7 @@ async def purge(event):
 
         await event.client.delete_messages(chat, msgs)
         del_res = await event.client.send_message(
-            event.chat_id, f"Purged {count} messages."
+            event.chat_id, f"Purged {count} messages.\n{time_taken} second(s)"
         )
 
         await asyncio.sleep(4)
@@ -75,7 +79,7 @@ async def purge(event):
         text = "Failed to delete messages.\n"
         text += "Messages maybe too old or I'm not admin! or dont have delete rights!"
         del_res = await event.respond(text, parse_mode="md")
-        await asyncio.sleep(5)
+        await asyncio.sleep(7)
         await del_res.delete()
 
 
@@ -93,8 +97,8 @@ async def delete_msg(event):
         return
     to_delete = event.message
     chat = await event.get_input_chat()
-    rm = [msg, to_delete]
-    await event.client.delete_messages(chat, rm)
+    remove = [msg, to_delete]
+    await event.client.delete_messages(chat, remove)
 
 
 __help__ = """
