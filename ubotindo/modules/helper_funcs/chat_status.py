604e8937a75f84eda30515718bad13330c1c9637
Tomy <tomy.prs25@gmail.com>
2020-10-12 20:30:15 +0700

    requirements.txt: Add hurry.filesize
    Syntax check
    
    Code Check
    
    Signed-off-by: Tomy <tomy.prs25@gmail.com>

--- ubotindo/modules/helper_funcs/chat_status.py
+++ ubotindo/modules/helper_funcs/chat_status.py
@@ -34,7 +34,9 @@ def is_user_ban_protected(
         or user_id in DEV_USERS
         or user_id in SUDO_USERS
         or user_id in WHITELIST_USERS
-        or chat.all_members_are_administrators or user_id in (777000, 1087968824):
+        or chat.all_members_are_administrators 
+        or user_id in int(777000, 1087968824)
+    ):
         return True
 
     if not member:
@@ -48,7 +50,7 @@ def is_user_admin(chat: Chat, user_id: int, member: ChatMember=None) -> bool:
         chat.type == "private"
         or user_id in DEV_USERS
         or user_id in SUDO_USERS
-        or user_id in int(777000, 1087968824)
+        or user_id in (777000, 1087968824)
         or chat.all_members_are_administrators
     ):
         return True
