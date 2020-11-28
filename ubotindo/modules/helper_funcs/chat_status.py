1509b9ebee2e6af5c47052f789ab174ec983df52
Tomy <tomy.prs25@gmail.com>
2020-11-28 07:12:52 +0700

    Add thread lock in user_admin decorator
    Co-Authored-By: starry69 <starry369126@outlook.com>
    Signed-off-by: Tomy <tomy.prs25@gmail.com>

--- ubotindo/modules/helper_funcs/chat_status.py
+++ ubotindo/modules/helper_funcs/chat_status.py
@@ -25,9 +25,11 @@ from ubotindo import (
     dispatcher,
 )
 from cachetools import TTLCache
+from threading import RLock
 
 # refresh cache 10m
 ADMIN_CACHE = TTLCache(maxsize=512, ttl=60 * 10)
+THREAD_LOCK = RLock()
 
 
 def can_delete(chat: Chat, bot_id: int) -> bool:
@@ -63,20 +65,21 @@ def is_user_admin(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
         return True
 
     if not member:
-        # try to fetch from cache first.
-        try:
-            return user_id in ADMIN_CACHE[chat.id]
-        except KeyError:
-            # keyerror happend means cache is deleted,
-            # so query bot api again and return user status
-            # while saving it in cache for future useage...
-            chat_admins = dispatcher.bot.getChatAdministrators(chat.id)
-            admin_list = [x.user.id for x in chat_admins]
-            ADMIN_CACHE[chat.id] = admin_list
+        with THREAD_LOCK:
+            # try to fetch from cache first.
+            try:
+                return user_id in ADMIN_CACHE[chat.id]
+            except KeyError:
+                # keyerror happend means cache is deleted,
+                # so query bot api again and return user status
+                # while saving it in cache for future useage...
+                chat_admins = dispatcher.bot.getChatAdministrators(chat.id)
+                admin_list = [x.user.id for x in chat_admins]
+                ADMIN_CACHE[chat.id] = admin_list
 
-            if user_id in admin_list:
-                return True
-            return False
+                if user_id in admin_list:
+                    return True
+                return False
 
 
 def is_bot_admin(
