8c2e8704ae1ea98ee16376f1eee427a09bfdae68
Mr.Miss <keselekpermen@mrmiss.wtf>
2020-12-22 17:14:09 +0700

    update header licensed repo
    Signed-off-by: Mr.Miss <keselekpermen@mrmiss.wtf>

--- ubotindo/modules/no_sql/__init__.py
+++ ubotindo/modules/no_sql/__init__.py
@@ -1,5 +1,5 @@
 # UserindoBot
-# Copyright (C) 2020  UserindoBot Team, <https://github.com/MoveAngel/UserIndoBot.git>
+# Copyright (C) 2020  UserindoBot Team, <https://github.com/userbotindo/UserIndoBot.git>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
